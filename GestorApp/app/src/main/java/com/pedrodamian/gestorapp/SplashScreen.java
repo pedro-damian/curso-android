447d88ff2903682461c17dad95d56a7a7f26868a
pedro-damian <136197537+pedro-damian@users.noreply.github.com>
2025-06-25 17:13:47 -0500

    Update files

--- GestorApp/app/src/main/java/com/pedrodamian/gestorapp/SplashScreen.java
+++ GestorApp/app/src/main/java/com/pedrodamian/gestorapp/SplashScreen.java
@@ -1,6 +1,8 @@
 package com.pedrodamian.gestorapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,22 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // esta funcion permite redireccionar de una actividad a otra en este caso de Splashscreen a Mainactivity
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+            // delaymillis es el tiempo para que ejecute la funcion run()
+        }, 4000);
+
+
     }
 }
