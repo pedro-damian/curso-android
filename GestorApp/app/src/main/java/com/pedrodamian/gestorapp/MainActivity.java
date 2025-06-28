b27e06e16be3df5aff56c7ea73cc84bbff0d0dc7
pedro-damian <136197537+pedro-damian@users.noreply.github.com>
2025-06-28 17:06:57 -0500

    Update files

--- GestorApp/app/src/main/java/com/pedrodamian/gestorapp/MainActivity.java
+++ GestorApp/app/src/main/java/com/pedrodamian/gestorapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.pedrodamian.gestorapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,13 +16,41 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Paso 1: declaro mi control con el tipo de elemento
+        Button btnweb;
+        Button btningresar;
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+        // Paso 2: ASOCIAR mi variable creada con el control de activity_main
+        btnweb = findViewById(R.id.btnWeb);
+        btningresar = findViewById(R.id.btnIngresar);
+
+        // Paso 3: Indicar a la variable que es lo que vas hacer
+        btnweb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toast es el mensajito tipo globito
+                // Toast.makeText(MainActivity.this, "Soy el boton", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, PaginaWeb.class));
+            }
         });
+
+        btningresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toast es el mensajito tipo globito
+                Toast.makeText(MainActivity.this, "Ingresate", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
---
+++ GestorApp/app/src/main/java/com/pedrodamian/gestorapp/PaginaWeb.java
@@ -0,0 +1,38 @@
+package com.pedrodamian.gestorapp;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PaginaWeb extends AppCompatActivity {
+
+    WebView wvpagina;
+    WebSettings webSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_pagina_web);
+
+        wvpagina = findViewById(R.id.wvPagina);
+        webSettings = wvpagina.getSettings();
+
+        // una web usa el DOM y el Javascript
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        // carga la url dentro de la actividad
+        wvpagina.loadUrl("https://platzi.com");
+        // atrapa y lo contiene dentro de la actividad
+        wvpagina.setWebViewClient(new WebViewClient());
+
+    }
+}
