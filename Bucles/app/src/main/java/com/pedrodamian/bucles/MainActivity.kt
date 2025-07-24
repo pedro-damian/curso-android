81cd50d607e093fabd0723745a0ef03c5135482c
pedro-damian <136197537+pedro-damian@users.noreply.github.com>
2025-07-24 17:14:05 -0500

    Update files

--- Bucles/app/src/main/java/com/pedrodamian/bucles/MainActivity.kt
+++ Bucles/app/src/main/java/com/pedrodamian/bucles/MainActivity.kt
@@ -8,9 +8,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    var nuevoIntento = false   // Interruptor que evita que el bucle continue indeterminadamente
+    var valorIntroducido = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,31 +27,32 @@ class MainActivity : AppCompatActivity() {
         val boton: Button = findViewById(R.id.btn_calcular)
         val resultado: TextView = findViewById(R.id.tv_resultado)
 
-
         // NUMERO AL AZAR (1 - 100)
         var numeroAleatorio = Random.nextInt(1,101)
-
+        // NUMERO DE INTENTOS
         var intentos = 0
 
-
-        resultado.text = numeroAleatorio.toString()
-
         boton.setOnClickListener{
-            intentos++
 
-            var numeroIngresado = numero.text.toString().toIntOrNull()
-
-            if (numeroIngresado==null || numeroIngresado<0 || numeroIngresado>100) {
-                resultado.text="Ingrese numero entre 1 y 100"
-            } else if (numeroIngresado < numeroAleatorio) {
-                resultado.text="El numero es mayor"
-            } else if (numeroIngresado > numeroAleatorio) {
-                resultado.text="El numero es menor"
-            } else {
-                resultado.text="Correcto!! Lo has logrado en ${intentos} intentos"
-            }
+            valorIntroducido= numero.text.toString().toInt()
+            nuevoIntento=true
 
         }
 
+        GlobalScope.launch(Dispatchers.Main) {
+            while (numeroAleatorio!=valorIntroducido){
+                if(nuevoIntento){
+                    intentos++
+                    if(numeroAleatorio<valorIntroducido) {
+                        resultado.text = "El numero es menor"
+                    } else if (numeroAleatorio>valorIntroducido) {
+                        resultado.text = "El numero es mayor"
+                    }
+                    nuevoIntento=false
+                }
+                delay(500)  // pausa
+            }
+            resultado.text="Correcto!! Lohas logrado en ${intentos} intentos"
+        }
     }
 }
